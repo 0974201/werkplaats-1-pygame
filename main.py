e98684f35a7a7e7194c29958b3d23620b1f6ad7b
Jeroen-1063146 <1063146@hr.nl>
2022-09-28 14:07:57 +0200

    general functions

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-print("Hello world!")
+from turtle import width
+import pygame
+from pygame import mixer
+import random
+import math
 
-print("enemy")
+# initialize pygame
+pygame.init()
 
-print("wowowowowowowowowowowwowowoowowowwowowowowoowowowowowowowwo")
+# set up the drawing window
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+background_color = (0, 0, 0)
+
+running = True
+
+while running:
+
+    # Background color
+    screen.fill(background_color)
+
+    # Quit game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
