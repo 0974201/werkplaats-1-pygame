9ac9a859f5ea059e5590faa5f4da5364a052771f
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-10-02 11:01:21 +0200

    test 3 (+ constructor)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from turtle import width
 import pygame
 from pygame import mixer
+#from turtle import width
 import random
 import math
 
@@ -13,6 +13,17 @@ height = 600
 screen = pygame.display.set_mode((width, height))
 background_color = (25, 25, 25) #als je dit ziet, zit je niet in de goede branch yo
 
+class Player(pygame.sprite.Sprite):
+    #uh ok, dus dit is constructor van de player.
+    #zeg maar de blauwprint van de player als ie in de game wordt aangeroepen.
+    def __init__(self, *groups: AbstractGroup):
+        super().__init__(*groups) #dank je vs code. ik had dit absoluut niet nodig.
+    print("test")
+
+
+##################
+
+player = Player() #hier roepen wij player aan.
 running = True
 
 while running:
