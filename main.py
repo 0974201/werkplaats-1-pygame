04e5e57976003978a20a2ba76ba7c9a3e331a117
Jeroen-1063146 <1063146@hr.nl>
2022-10-02 16:36:39 +0200

    Feature: start and exit buttons (with temp images)

--- main.py
+++ main.py
@@ -87,14 +87,70 @@ height = 600
 screen = pygame.display.set_mode((width, height))
 background_color = (0, 0, 0)
 
-running = True
+# Load button images
+start_img = pygame.image.load("assets/images/start.png")
+exit_img = pygame.image.load("assets/images/exit.png")
+
+#Title
+pygame.display.set_caption("Space Warriors")
+
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+        
+    def draw(self):
+        action = False
+        
+        # Mouse position
+        mouse_pos = pygame.mouse.get_pos()
+        
+        # Check if mouse is over the button
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        return action
+
+# Create buttons
+start_button = Button(100, 200, start_img, 0.5)
+exit_button = Button(450, 200, exit_img, 0.5)
+
+start = True
+
+while start:
+    
+    # Background color
+    screen.fill(background_color)
+    
+    if start_button.draw():
+        print("Start")
+    if exit_button.draw():
+        print("Exit")
+        
+    # Quit game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start = False
+            
+    pygame.display.update()
+    
 
 while running:
 
     # Background color
-    screen.fill(background_color)
+    screen.fill((255, 255, 255))
 
     # Quit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+pygame.quit()
