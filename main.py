447a3955bea999b97e85ea1da8361aeb5befc510
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-10-04 00:22:10 +0200

    player added. todo: movement logic

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
-#from pygame import mixer
-#from turtle import width
-#import random
-#import math
+from pygame import *
 
+#const var, hier zitten wij verder niet meer aan.
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+PLAYER_COLOUR = (148, 24, 24)
+BACKGROUND_COLOUR = (217, 176, 176) #note to self; verander dit weer terug naar zwart. problem is fixed.
 
 class Player(pygame.sprite.Sprite):
     #uh ok, dus dit is constructor van de player.
@@ -11,26 +13,27 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self.image = pygame.Surface([width, height]) #dit maakt dus de image (in dit geval een blok) voor de player object aan
+        self.image.fill(color) #hier krijgt t een kleurtje, kan ook achterwege gelaten worden als er een img file gebruikt wordt.
 
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect() #haalt op wat er in bovenstaande regels is aangemaakt
 
-    print("test") #check of hij aangeroepen wordt.
+    def move(self, x, y):
+        return "todo"
+
+    def update(self):
+        return "todo"
 
 # initialize pygame
 pygame.init()
 
 # set up the drawing window
-width = 800
-height = 600
-screen = pygame.display.set_mode((width, height))
-player_colour = (148, 24, 24)
-background_color = (217, 176, 176) #als je dit ziet, zit je niet in de goede branch yo
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Space Warriors")
 
-##################
-
-player = Player(player_colour, 50, 50) #hier roepen wij player aan. hopelijk pakt ie hex colour
+#gaem objects
+player = Player(PLAYER_COLOUR, 55, 55) #hier roepen wij player aan.
+player.rect.y = 250 #wordt op 250px gespawned
 player_list = pygame.sprite.Group() #hier gaat de sprite voor player in
 player_list.add(player) #en is nu toegevoegd.
 
@@ -40,12 +43,24 @@ running = True
 while running:
     
     # Background color
-    screen.fill(background_color)
-
+    screen.fill(BACKGROUND_COLOUR)
+    
     # Quit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                print("up")
+            if event.key == pygame.K_DOWN:
+                print("down")
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                print("up released")
+            if event.key == pygame.K_DOWN:
+                print("down released")
+
     player_list.draw(screen) #alleen is ie niet op t scherm, maar hij pakt de veranderde kleurwaardes van background ook niet. wat.
+    pygame.display.flip() #oh joh. okay. ik had dit dus nodig. dit updatetedtdtft het hele scherm met alles wat er tot nu toe is toegevoegd. (als ik het goed begrijp)
