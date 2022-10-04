2c6dccb5888912155f7ce1782f64e35884468f93
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-10-04 12:36:36 +0200

    got player movement working

--- main.py
+++ main.py
@@ -5,38 +5,42 @@ from pygame import *
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 PLAYER_COLOUR = (148, 24, 24)
-BACKGROUND_COLOUR = (217, 176, 176) #note to self; verander dit weer terug naar zwart. problem is fixed.
+BACKGROUND_COLOUR = (31, 29, 29) 
+#BACKGROUND_COLOUR = (0x1F1D1D)
 
+#player class:
 class Player(pygame.sprite.Sprite):
     #uh ok, dus dit is constructor van de player.
     #zeg maar de blauwprint van de player als ie (in de game) wordt aangeroepen.
     def __init__(self, color, width, height):
         pygame.sprite.Sprite.__init__(self)
-
+    
         self.image = pygame.Surface([width, height]) #dit maakt dus de image (in dit geval een blok) voor de player object aan
         self.image.fill(color) #hier krijgt t een kleurtje, kan ook achterwege gelaten worden als er een img file gebruikt wordt.
-
         self.rect = self.image.get_rect() #haalt op wat er in bovenstaande regels is aangemaakt
-
+        
         self.changeX = 0
         self.changeY = 0
 
     def move(self, x, y):
         self.changeX += x
         self.changeY += y
-        #return "todo"
 
     def update(self):
-        self.rect.x += self.changeX
+        self.rect.x += self.changeX #update positie op het scherm
         self.rect.y += self.changeY
         
+        if self.rect.y < 0:
+            self.rect.y = 0
+
         if self.rect.x < 0:
             self.rect.x = 0
 
         if self.rect.x > SCREEN_WIDTH - self.rect.width:
             self.rect.x = SCREEN_WIDTH - self.rect.width
 
-        #return "todo"
+        if self.rect.y > SCREEN_HEIGHT - self.rect.height:
+            self.rect.y = SCREEN_HEIGHT - self.rect.height #dit zorgt ervoor dat hij in het scherm blijft
 
 # initialize pygame
 pygame.init()
@@ -50,7 +54,6 @@ player = Player(PLAYER_COLOUR, 50, 50) #hier roepen wij player aan.
 player.rect.y = 250 #wordt op 250px gespawned
 player_list = pygame.sprite.Group() #hier gaat de sprite voor player in
 player_list.add(player) #en is nu toegevoegd.
-
 
 running = True
 
@@ -66,17 +69,32 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                player.move(50,-50)
+                player.move(0, -2)
                 print("up")
             if event.key == pygame.K_DOWN:
-                player.move(-50,50)
+                player.move(0, 2)
                 print("down")
+            if event.key == pygame.K_LEFT:
+                player.move(-2, 0)
+                print("left")
+            if event.key == pygame.K_RIGHT:
+                player.move(2, 0)
+                print("right")
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
+                player.move(0, 0)
                 print("up released")
             if event.key == pygame.K_DOWN:
+                player.move(0, 0)
                 print("down released")
+            if event.key == pygame.K_LEFT:
+                player.move(0, 0)
+                print("left released")
+            if event.key == pygame.K_RIGHT:
+                player.move(0, 0)
+                print("right released")
 
     player_list.draw(screen) #alleen is ie niet op t scherm, maar hij pakt de veranderde kleurwaardes van background ook niet. wat.
-    pygame.display.update() #oh joh. okay. ik had dit dus nodig. dit updatetedtdtft het hele scherm met alles wat er tot nu toe is toegevoegd. (als ik het goed begrijp)
+    player_list.update() #was dit vergeten toe te voegen, nu kunnen we de player zien bewegen op het scherm
+    pygame.display.flip() #oh joh. okay. ik had dit dus nodig. dit updatetedtdtft het hele scherm met alles wat er tot nu toe is toegevoegd. (als ik het goed begrijp)
