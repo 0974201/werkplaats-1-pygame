e18ac07387b7eb9a17d85aef1a2fd35e67cf2651
Jeroen-1063146 <1063146@hr.nl>
2022-10-05 12:22:12 +0200

    Added classes and file for enemies

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from turtle import width
 import pygame
 from pygame import mixer
+from enemy import Enemy
+from enemy import EnemyGroup
 import random
 import math
 
 # initialize pygame
 pygame.init()
-
 
 # set up the drawing window
 width = 800
@@ -18,23 +19,9 @@ background_color = (0, 0, 0)
 # Bunyamin/ eerste enemys:
 
 # Enemy (coordinaten nog aanpassen naar side shooter)
-enemyImg = []
-enemyX = []
-enemyY = []
-enemyX_change = []
-enemyY_change = []
-num_of_enemy = 10
-
-for i in range(num_of_enemy):
-    enemyImg.append(pygame.image.load('assets/images/alienvijand.png'))
-    enemyX.append(random.randint(0, 735))
-    enemyY.append(random.randint(50, 150))
-    enemyX_change.append(0.5)
-    enemyY_change.append(20)
-
-
-def enemy(x, y, i):
-    screen.blit(enemyImg[i], (x, y))
+    
+enemy = Enemy(0, 0, 0.5)
+enemyGroup = EnemyGroup(10)
 
 
 # game loop
@@ -45,7 +32,8 @@ while running:
     screen.fill(background_color)
 
     # hierboven moet de while loop komen
-    enemy(enemyX[i], enemyY[i], i)
+    # enemy(enemyX[i], enemyY[i], i)
+    enemyGroup.update(screen)
 
     # Quit game
     for event in pygame.event.get():
@@ -54,15 +42,5 @@ while running:
 
     # Enemy movement/bewegingen (coordinaten nog aanpassen) de colission
     #moet hieronder komen!
-    for i in range(num_of_enemy):
-        enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 0.5
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 735:
-            enemyX_change[i] = -0.5
-            enemyY[i] += enemyY_change[i]
 
-
-        enemy(enemyX[i], enemyY[i], i)
     pygame.display.update()
