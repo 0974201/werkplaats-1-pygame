cb55bb0a64e340443c64dcf2c7f2fa6b8c2b7fd3
Nizar-1012373 <113017403+Nizar-1012373@users.noreply.github.com>
2022-10-10 16:36:59 +0200

    enemy plaatje van Nizar er in gezet

--- enemy.py
+++ enemy.py
@@ -2,15 +2,17 @@ import pygame
 import random
 from pygame.sprite import Sprite, Group, collide_rect
 
+
 class Enemy(Sprite):
     def __init__(self, x, y, speed):
         super().__init__()
-        self.image = pygame.image.load('assets/images/alienvijand.png')
+        self.image = pygame.image.load("assets/images/enemy_4.png")
+        self.image = pygame.transform.scale(self.image, (32, 32))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.speed = speed
-    
+
     def move(self):
         self.rect.y += self.speed
         if self.rect.y <= 0:
@@ -23,14 +25,15 @@ class Enemy(Sprite):
     def update(self, screen):
         self.move()
         screen.blit(self.image, self.rect)
-        
+
+
 class EnemyGroup(Group):
     def __init__(self, num_of_enemy):
         super().__init__()
         for i in range(num_of_enemy):
             enemy = Enemy(random.randint(350, 750), random.randint(0, 350), 1.0)
             self.add(enemy)
-            
+
     def move(self):
         for enemy in self.sprites():
             enemy.move()
